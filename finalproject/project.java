b2b12541e5bf1c4541bda75503deba22ba448afb
Benoy Thomas <122180199+bthoma31@users.noreply.github.com>
2023-11-30 12:28:44 -0500

    Update finalproject

--- finalproject/project.java
+++ finalproject/project.java
@@ -1,50 +1,64 @@
 package finalproject;
 
 import java.util.Hashtable;
+import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class project {
     public static void main(String[] args) {
         System.out.println("Welcome to Bik & Noy's Schedule Comparer");
-        System.out.println("Choose an Option:");
-        System.out.println(" 1.  Enter your class & time");
-        System.out.println(" 2.  Add a part");
-
-    }
-
-    public static void array() {
-
-    }
-
-    public static void Stack() {
-
-    }
-
-    public static void Hashmap() {
         Scanner keyboard = new Scanner(System.in);
-        Hashtable<String, String> c1 = new Hashtable<String, String>();
-        while (true) {
-            System.out.println("Enter a class name:");
-            String className = keyboard.nextLine();
-            System.out.println("Enter your class time: ex. 11");
-            String section = keyboard.nextLine();
-            c1.put(className, section);
-            System.out.println("Do you want to keep adding classes?");
-            String quit = keyboard.nextLine();
-            if (quit.equalsIgnoreCase("yes")) {
-                continue;
-            } else {
-                break;
+        System.out.println("Fill out your schedule to continue");
+        createSchedule();
+
+    }
+
+    // linked list that stores students you want to compare and then stores the
+    // classes and times for each student in a hashtable
+    public static void createSchedule() {
+        Scanner keyboard = new Scanner(System.in);
+        LinkedList<student> s1 = new LinkedList<>();
+        System.out.println("How many people do you want to input?");
+        int num = keyboard.nextInt();
+        for (int i = 0; i < num; i++) {
+            System.out.print("Enter name:");
+            keyboard.nextLine();
+            String name = keyboard.nextLine();
+            student st = new student(name);
+            s1.add(st);
+            System.out.println("How many classes does the student have?");
+            int classNum = keyboard.nextInt();
+            Stack obj = new Stack<String[]>();
+            for (int j = 0; j < classNum; j++) {
+                System.out.println("Enter name of the class");
+                keyboard.nextLine();
+                String className = keyboard.nextLine();
+                System.out.println("Enter time of the class as an integer(in terms of World time so 9pm would be 21)");
+                int time = keyboard.nextInt();
+                st.addToSchedule(className, time);
             }
+            System.out.println(st.getSortedSchedule());
 
         }
-    }
-
-    public static void Queue() {
 
     }
 
-    public static void LinkedList() {
+    // method that prints out the linkedlist if the user wants too
+    public static void printStudents(LinkedList<student> s) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Would you like to see the students?");
+        String ans = keyboard.nextLine();
+        if (ans.equalsIgnoreCase("yes")) {
+            System.out.println(s);
+        }
 
     }
+
+    public static void getOptions() {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Choose an option:");
+
+    }
+
 }
--- finalproject/student.java
+++ finalproject/student.java
@@ -1,6 +1,71 @@
 package finalproject;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 public class student {
   public String name;
-  public int id;
+  public Hashtable<String, Integer> schedule;
+
+  public student(String name) {
+    this.name = name;
+    this.schedule = new Hashtable<>();
+  }
+
+  public void addToSchedule(String className, int classTime) {
+    schedule.put(className, classTime);
+  }
+
+  public void printSchedules() {
+
+  }
+
+  // method that takes class times and stores it in an array and then bubble sorts
+  // it into queue
+  public Queue<String> getSortedSchedule() {
+    ArrayList<String[]> temp = new ArrayList<>(); // queue
+
+    Enumeration<String> keys = schedule.keys();
+    while (keys.hasMoreElements()) {
+      String key = keys.nextElement();
+      String[] keyValuePair = new String[] { key, schedule.get(key).toString() }; // array to take the keys and the
+                                                                                  // values of the hashmap and combine
+                                                                                  // them into one array pair and stores
+                                                                                  // into an arraylist
+      temp.add(keyValuePair);
+    }
+
+    // bubble sort the arraylist by time and put into queue in right order
+
+    for (int i = temp.size() - 1; i > 0; i--) {
+      for (int j = 0; j < i; j++) {
+        String[] pair1 = temp.get(j);
+        String[] pair2 = temp.get(j + 1);
+
+        if (Integer.parseInt(pair1[1]) > Integer.parseInt(pair2[1])) {
+          // Swap the elements
+          String[] tempPair = pair1;
+          temp.set(j, pair2);
+          temp.set(j + 1, tempPair);
+        }
+      }
+    }
+
+    // Create an empty Queue to store the sorted schedule
+    Queue<String> sortedSchedule = new LinkedList<>();
+
+    // Iterate through the sorted ArrayList and add the class names to the Queue
+    for (String[] pair : temp) {
+      sortedSchedule.add(pair[0]);
+    }
+
+    return sortedSchedule;
+  }
+
 }
